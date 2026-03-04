0571c44279541e59f52152197e6fbf482a344295
DevDodi <8mohammedmousa4@gmail.com>
2026-03-04 19:58:48 +1100

    Added user api functions

---
+++ front-end/task-management-ui/src/api/users.api.js
@@ -0,0 +1,18 @@
+import { apiClient } from './httpClient.js';
+
+export async function getUser(id) {
+  return apiClient.get(`/users/${id}`);
+}
+
+export async function createUser(user) {
+  return apiClient.post('/users', user);
+}
+
+export async function updateUser(id, user){
+  return apiClient.put(`/users/${id}`, user)
+}
+
+export async function deleteUser(id){
+  return apiClient.delete(`/users/${id}`)
+}
+
