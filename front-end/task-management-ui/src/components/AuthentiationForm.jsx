325c7478874f7ce227241dc0c009e80f156d2641
DevDodi <8mohammedmousa4@gmail.com>
2026-03-06 18:37:43 +1100

    Fixed incorrect style being used for all pages

--- front-end/task-management-ui/src/components/AuthentiationForm.jsx
+++ front-end/task-management-ui/src/components/AuthentiationForm.jsx
@@ -4,15 +4,17 @@ import "../pages/css/Register.css";
 export default function AuthenticationForm({ header, value, onSubmit , error, buttonText, children}) {
 
     return(
-        <div className="container">
-            <form onSubmit={onSubmit} style={{ width: "300px" }}>
-                <h2 className="header">{header}</h2>
-                {children}
-                {error && <div style={{ color: "red", marginBottom: "10px", textAlign: "center" }}>{error}</div>}
-                <button className="actionButton" type="submit">
-                    {buttonText}
-                </button>
-            </form>
+        <div className="authWrapper">
+            <div className="container">
+                <form onSubmit={onSubmit} style={{ width: "300px" }}>
+                    <h2 className="header">{header}</h2>
+                    {children}
+                    {error && <div style={{ color: "red", marginBottom: "10px", textAlign: "center" }}>{error}</div>}
+                    <button className="actionButton" type="submit">
+                        {buttonText}
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
