69426f59d0b66dee27705d925efca874ec372910
DevDodi <8mohammedmousa4@gmail.com>
2026-03-06 18:52:06 +1100

    Added skeleton Projects page

--- front-end/task-management-ui/src/pages/Projects.jsx
+++ front-end/task-management-ui/src/pages/Projects.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./css/Projects.css";
+
 export default function Projects() {
 
-
+    return (
+        <div className="projectsContainer">
+            <h1>Projects</h1>
+            <p>Welcome to the Projects page!</p>
+            <table>
+                
+            </table>
+        </div>
+    );
 
 }
