4c4b5752a90677096e790fb241be105c3fc16d4d
DevDodi <8mohammedmousa4@gmail.com>
2026-03-22 03:20:08 +1100

    Can now edit and create tasks (Project Details WIP)
    - Edit and Create options are now mostly usable
    - Modals updated to show inputs for these actions

--- front-end/task-management-ui/src/pages/ProjectDetails.jsx
+++ front-end/task-management-ui/src/pages/ProjectDetails.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getProjectById } from "../api/projects.api.js";
-import { getProjectTasks } from "../api/tasks.api.js";
+import { createTask, getProjectTasks, updateTask } from "../api/tasks.api.js";
 import { getUser } from "../api/users.api.js";
 import Modal from "../components/Modal";
 import TaskRow from "../components/TaskRow";
+import {getStatusLabel} from "../components/TaskRow";
+import {TaskStatus} from "../api/types/models.js";
 import "./css/ProjectDetails.css";
 
 
@@ -21,31 +23,37 @@ export default function ProjectDetails() {
     const [showEditModal, setShowEditModal] = useState(null);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [assignee, setAssignee] = useState('00000000-0000-0000-0000-000000000000');
+    const [status, setStatus] = useState(0);
+
+    const fetchTasks = async () => {
+        const response = await getProjectTasks(id);
+        const tasksWithExtras = await Promise.all(
+            (response.tasks ?? []).map(async (task) => {
+                let assigneeEmail = "Unassigned";
+                if (task.assignedUser) {
+                    try {
+                        const userResponse = await getUser(task.assignedUser);
+                        assigneeEmail = userResponse?.user.email ?? assigneeEmail;
+                    } catch { /* eat exception and keep default*/}
+                }
+
+                return {
+                    ...task,
+                    assigneeEmail,
+                };
+            })
+        );
+        return tasksWithExtras;
+    }
 
     useEffect(() => {
         const fetchProjectAndTasks = async () => {
             try {
-                let response = await getProjectById(id);
+                const response = await getProjectById(id);
                 setSelectedProject(response.project);
 
-                response = await getProjectTasks(response.project.id);
-                const tasksWithExtras = await Promise.all(
-                    (response.tasks ?? []).map(async (task) => {
-                        let assigneeEmail = "Unassigned";
-                        if (task.assignedUser) {
-                            try {
-                                const user = await getUser(task.assignedUser);
-                                assigneeEmail = user?.email ?? assigneeEmail;
-                            } catch { /* eat exception and keep default*/}
-                        }
-
-                        return {
-                            ...task,
-                            assigneeEmail,
-                        };
-                    })
-                );
-
+                const tasksWithExtras = await fetchTasks();
                 setTasks(tasksWithExtras);
             } catch (err) {
                 setError("Error fetching project and tasks");
@@ -57,17 +65,30 @@ export default function ProjectDetails() {
     }, [id]);
 
     const handleCreate = async () => {
-        // const projectId = crypto.randomUUID();
-        // const response = await createProject({Id: projectId, Name: title, Description: description, OwnerId: userId});
-        // setShowCreateModal(false);
-        // navigate(`/projects/${projectId}`);
+        const taskId = crypto.randomUUID();
+        await createTask({
+            Id: taskId,
+            Title: title,
+            Description: description,
+            AssignedUser: assignee,
+            Status: status,
+            AssignedProject: id, // TODO: add choice of changing task to different project
+        });
+        setShowCreateModal(false);
+        fetchTasks().then(setTasks);
     }
 
     const handleUpdate = async () => {
-        // const projectId = crypto.randomUUID();
-        // const response = await createProject({Id: projectId, Name: title, Description: description, OwnerId: userId});
-        // setShowCreateModal(false);
-        // navigate(`/projects/${projectId}`);
+        await updateTask(selectedTask.id, {
+            Id: selectedTask.id,
+            Title: title,
+            Description: description,
+            AssignedUser: assignee,
+            Status: status,
+            AssignedProject: id, // TODO: add choice of changing task to different project
+        });
+        setShowEditModal(false);
+        fetchTasks().then(setTasks);
     }
 
     if (loading) return <div className="detailsContainer">Loading...</div>;
@@ -142,12 +163,51 @@ export default function ProjectDetails() {
                         placeholder="Enter task description (optional)"
                         onChange={(e) => setDescription(e.target.value)}
                     />
+                    <input 
+                        className="modalAssignee"
+                        type="text" 
+                        placeholder="Enter assignee's email"
+                        onChange={(e) => setAssignee(e.target.value)}
+                    />
+                    <select className="modalStatus" onChange={(e) => setStatus(parseInt(e.target.value))}>
+                        {Object.values(TaskStatus).map((v) => (
+                            <option key={v} value={v}>
+                                {getStatusLabel(v)}
+                            </option>
+                        ))}
+                    </select>
                 </Modal>
             )}
 
             {showEditModal && (
                 <Modal title="Edit Task" onClickOutside={() => setShowEditModal(false)} onClickAction={handleUpdate}>
-                    
+                    <input 
+                        className="modalInput"
+                        type="text" 
+                        placeholder="Enter task name"
+                        defaultValue={selectedTask.title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        className="modalDescription"
+                        placeholder="Enter task description (optional)"
+                        defaultValue={selectedTask.description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <input 
+                        className="modalAssignee"
+                        type="text" 
+                        placeholder="Enter assignee's email"
+                        defaultValue={selectedTask.assigneeEmail == "Unassigned" ? "" : selectedTask.assigneeEmail}
+                        onChange={(e) => setAssignee(e.target.value)}
+                    />
+                    <select className="modalStatus" onChange={(e) => setStatus(parseInt(e.target.value))} defaultValue={selectedTask.status}>
+                        {Object.values(TaskStatus).map((v) => (
+                            <option key={v} value={v}>
+                                {getStatusLabel(v)}
+                            </option>
+                        ))}
+                    </select>
                 </Modal>
             )}
 
