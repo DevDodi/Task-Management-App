31a16ed5826b35c049b36effca2d0005fd04cc9f
DevDodi <8mohammedmousa4@gmail.com>
2026-03-22 03:16:27 +1100

    Updated colours used for UI
    - used green and beige colours

--- front-end/task-management-ui/src/pages/Login.jsx
+++ front-end/task-management-ui/src/pages/Login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
     <AuthenticationForm header="Login" onSubmit={handleSubmit} error={error} buttonText="Sign in">
       <Input
         type="email"
-        style={{ backgroundColor: "white", color: "black" }}
+        style={{ backgroundColor: "#FFF4e2", color: "black" }}
         placeholder="Email"
         value={email}
         icon = {emailIcon}
@@ -49,7 +49,7 @@ export default function Login() {
       />
       <Input
         type="password"
-        style={{ backgroundColor: "white", color: "black" }}
+        style={{ backgroundColor: "#FFF4e2", color: "black" }}
         placeholder="Password"
         value={password}
         icon = {padlockIcon}
