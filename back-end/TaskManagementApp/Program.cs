9d57983d6219c5b1779fe5cc0c918b984a884a84
DevDodi <8mohammedmousa4@gmail.com>
2026-04-08 17:15:24 +1000

    Add security headers (feedback)

--- back-end/TaskManagementApp/Program.cs
+++ back-end/TaskManagementApp/Program.cs
@@ -84,6 +84,15 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.Use(async (context, next) =>
+{
+    context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+    context.Response.Headers["X-Frame-Options"] = "DENY";
+    context.Response.Headers["X-XSS-Protection"] = "1; mode=block";
+    context.Response.Headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains";
+    await next();
+});
+
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
 
